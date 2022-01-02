096f84f3eaecb42d72621df8d511bdd126835b88
jjtroiano0272@eagle.fgcu.edu <jjtroiano0272@eagle.fgcu.edu>
2022-01-01 19:37:53 -0500

    init commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,66 @@
+import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 import Die from './Die';
 
 export default function Main(props) {
-  const rollResult = ((Math.random() * 10) % 6).toPrecision(1);
   const [diceValues, setDiceValues] = useState(allNewDice(10));
 
   function allNewDice(numDice) {
-    return Array.from({ length: numDice }, () => Math.ceil(Math.random() * 6));
+    // return Array.from({ length: numDice }, () => Math.ceil(Math.random() * 6));
+    const newDiceArr = [];
+
+    function toggleHold() {
+      newDiceArr;
+    }
+
+    // TODO: Can this be more elegant? Array.from
+    for (let i = 0; i < numDice; i++) {
+      newDiceArr.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
+    }
+
+    return newDiceArr;
+  }
+
+  function toggleHold() {
+    console.log('die clicked');
   }
 
   return (
     <div className='container'>
       <div className='container bg-light rounded text-dark text-center p-5'>
         <h1>Tenzies</h1>
-        <div className='row my-2'>
-          {/* map render 5 here */}
-          {/* TODO: This might cause some issues */}
-          {/* TODO: Offload this into a Dice.jsx component that captures Die.jsx and 
+        {/* map render 5 here */}
+        {/* TODO: This might cause some issues */}
+        {/* TODO: Offload this into a Dice.jsx component that captures Die.jsx and 
                     takes numRows, numCols params, so that this below formatting will 
                     automatically be determined  */}
-          {allNewDice(5).map((item) => (
-            <div className='col'>
-              <button className='btn btn-md btn-secondary'>
-                <Die key={item} value={item.toString()} />
+        <div className='row my-2'>
+          {allNewDice(5).map((die) => (
+            <div className='col' key={die.id}>
+              <button
+                className='btn btn-md btn-secondary'
+                onClick={() => {
+                  die.isHeld(!die.isHeld);
+                  console.log(die.isHeld);
+                }}
+              >
+                <Die value={JSON.stringify(die.value)} />
               </button>
             </div>
           ))}
         </div>
         <div className='row my-2'>
-          {allNewDice(5).map((item) => (
-            <div className='col'>
-              <button className='btn btn-md btn-secondary'>
-                <Die key={item} value={item.toString()} />
+          {allNewDice(5).map((die) => (
+            <div className='col' key={die.id}>
+              <button
+                className='btn btn-md btn-secondary'
+                onClick={() => toggleHold()}
+              >
+                <Die value={die.value} />
               </button>
             </div>
           ))}
